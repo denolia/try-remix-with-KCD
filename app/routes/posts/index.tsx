eddf05419651e2570c274acca468d3b1a3dc37cb
Julia Bubnova <bubnova.j.i@gmail.com>
2022-11-14 15:10:47 +0100

    Add types for the data loader

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,19 +1,24 @@
 import { Link, useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { getPosts } from "~/models/post.server";
 
-export const loader = async () => {
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPosts>>;
+};
+
+export const loader: LoaderFunction = async () => {
   const posts = await getPosts();
-  return json({ posts });
+  return json<LoaderData>({ posts });
 };
 
 export default function PostsRoute() {
-  const { posts } = useLoaderData();
+  const { posts } = useLoaderData() as LoaderData;
   return (
     <main>
       <h1>Posts</h1>
       <ul>
-        {posts.map((post: any) => (
+        {posts.map((post) => (
           <li key={post.slug}>
             <Link to={post.slug} className="text-blue-600 underline">
               {post.title}
